04a7595b8b338ec5dd5c904af0b24aac14bb4073
Pranjul Chaurasiya <chaurasiyapranjul@gmail.com>
2025-03-27 04:16:13 +0530

    alltask initialized

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Header/Header";
 import CreateTask from "../CreateTask/CreateTask";
+import Alltask from "../Alltask/Alltask";
 
 const AdminDashboard = () => {
   return (
     <div className="min-h-screen w-full bg-gray-100">
       <Header />
       <CreateTask/>
+      <Alltask/>
     </div>
   );
 };
